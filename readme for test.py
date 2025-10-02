38d0655b631badceca27680c29472f7cccf65401
eknight26 <146714861+eknight26@users.noreply.github.com>
2025-10-02 23:50:37 +1300

    Create readme for test.py

---
+++ readme for test.py
@@ -0,0 +1,12 @@
+READ ME for test.py
+
+
+Ensure current folder contains relevant files/folder
+	ie. ./testdata/, test.py, etc
+
+To run script -
+
+on command terminal type:
+python test.py model.pth
+
+and hit ENTER
